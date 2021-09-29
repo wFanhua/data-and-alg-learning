fc4a0c75e15c24ee22e4d7f06e3806f5ca95fa1b
wangfanhua <wfanhua@outlook.com>
2021-09-29 17:44:29 +0800

    feat: sort

---
+++ src/alg/merge-sort/index.ts
@@ -0,0 +1,36 @@
+export class MergeSort {
+  sort(arr: Array<number>) {
+    this.sortR(arr, 0, arr.length - 1)
+  }
+  sortR(arr: Array<number>, l: number, r: number) {
+    if (l >= r) {
+      return
+    }
+    const mid = Math.floor((l + r) / 2)
+    this.sortR(arr, l, mid)
+    this.sortR(arr, mid + 1, r)
+    if (arr[mid] > arr[mid + 1]) {
+      this.merger(arr, l, mid, r)
+    }
+  }
+  merger(arr: Array<number>, l: number, mid: number, r: number) {
+    const copyArr = arr.slice(l, r + 1)
+    let i = l
+    let j = mid + 1
+    for (let k = l; k <= r; k++) {
+      if (i > mid) {
+        arr[k] = copyArr[j - l]
+        j++
+      } else if (j > r) {
+        arr[k] = copyArr[i - l]
+        i++
+      } else if (copyArr[i - l] <= copyArr[j - l]) {
+        arr[k] = copyArr[i - l]
+        i++
+      } else {
+        arr[k] = copyArr[j - l]
+        j++
+      }
+    }
+  }
+}
